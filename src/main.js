9684728bf67ab9ae264fd94c1cdc3809f9666f52
Redon <92redon@gmail.com>
2020-02-02 00:58:19 +0100

    test commit 2

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+// import router from './router'
 import './registerServiceWorker'
+import Home from './views/Home.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
+  Home,
   render: h => h(App)
 }).$mount('#app')
