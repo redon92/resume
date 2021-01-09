4428398fb2a7c8de082e881879d0e087be95a7d7
Redon <redon.lacaj@rightsearch.de>
2021-01-09 14:49:42 +0100

    pf

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.106427ebe1e027514d482f0c540ad058.js"
+  "/precache-manifest.749730b35ba2569b70c781e5955adcd1.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "redon-resume"});
